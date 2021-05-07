4f553050a78aa6fc385d4a0bef77cc1e2a3b2855
MarceloRodriguezSanti <83610104+MarceloRodriguezSanti@users.noreply.github.com>
2021-05-06 21:48:33 -0300

    Add files via upload

---
+++ TP1/Ejercicio 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "ingrese una velocidad en m/s: " << endl;
+    cin >> x;
+    cout << "la velocidad en km/hr es igual a: " << x*3.6 << endl;
+    return 0;
+}
